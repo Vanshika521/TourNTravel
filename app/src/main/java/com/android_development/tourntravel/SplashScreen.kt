a17abe2c3a0b50d4250d32848786e4a29af5f3ab
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2025-07-21 22:56:04 +0530

    Designing

--- app/src/main/java/com/android_development/tourntravel/SplashScreen.kt
+++ app/src/main/java/com/android_development/tourntravel/SplashScreen.kt
@@ -3,10 +3,7 @@ package com.android_development.tourntravel
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
     private lateinit var txtView1: TextView
     private lateinit var txtView2: TextView
@@ -21,18 +18,12 @@ class SplashScreen : AppCompatActivity() {
         txtView2 = findViewById(R.id.txtView2)
 
         main()
-
         }
     fun main(){
         txtView2.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
-
-        }
-
-
-
     }
+}
 
