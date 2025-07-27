99ffa6043cc6ac8d3e66b0cf8f01d5185e2e6c70
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2025-07-27 23:42:57 +0530

    Designing

--- app/src/main/java/com/android_development/tourntravel/SplashScreen.kt
+++ app/src/main/java/com/android_development/tourntravel/SplashScreen.kt
@@ -4,26 +4,35 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.android_development.tourntravel.databinding.ActivitySplashScreenBinding
 
-    private lateinit var txtView1: TextView
-    private lateinit var txtView2: TextView
 
 class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding :ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_splash_screen)
 
-        txtView1 = findViewById(R.id.txtView1)
-        txtView2 = findViewById(R.id.txtView2)
+        binding= ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        main()
+        binding.apply {
+
         }
-    fun main(){
-        txtView2.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+
+//        txtView1 = findViewById(R.id.txtView1)
+//        txtView2 = findViewById(R.id.txtView2)
+//
+//        main()
+//        }
+//    fun main(){
+//        txtView2.setOnClickListener {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
 
